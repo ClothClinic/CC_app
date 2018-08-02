4e8ce8b4eafe30b96ec13819eb9fbb3a9ba8e2fb
Bhumika Saini <bs98.saini@gmail.com>
2018-08-02 20:53:21 +0530

    Connected Sign Up & Sign In

---
+++ HomeScreen.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
+
+export default class HomeScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <Text>BLa</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00b5ec',
+  },
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  signupButton: {
+    backgroundColor: "#000000",
+  },
+  signUpText: {
+    color: 'white',
+  }
+});
+ 
